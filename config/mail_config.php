1f78acdca90325a247b95cf6baf40a11a35abe15
PeterBoh <peterboh67@gmail.com>
2026-02-17 13:20:01 +0100

    Update mail_config.php

--- config/mail_config.php
+++ config/mail_config.php
@@ -2,7 +2,7 @@
 // SMTP Zugangsdaten (NICHT öffentlich!)
 $SMTP_HOST = 'smtp.strato.de';
 $SMTP_USER = 'kontakt@innovation2000.de';
-$SMTP_PASS = 'Inno-2026;;PB';
+$SMTP_PASS = '';
 
 $SMTP_PORT = 587;     // alternativ 465
 $SMTP_ENCRYPTION = 'tls'; // alternativ 'ssl'
